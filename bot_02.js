1bcf502e9f04a2c4a5d5364b4e6c65d355b23dff
TARCISIO NUNES <tarcnux@gmail.com>
2020-10-21 20:28:54 -0300

    Bot responder ao comando /start

---
+++ bot_02.js
@@ -0,0 +1,24 @@
+const env = require('./.env')
+const Telegraf = require('telegraf')
+const bot = new Telegraf(env.token)
+
+bot.use(async (ctx, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date - start
+    const dataEHora = new Date().toLocaleString();
+    console.log(`${dataEHora} \n Tempo de resposta: ${ms}ms`)
+})
+
+//Escuta do comando /start
+bot.start(async ctx => {
+    const from = ctx.message.from
+    from.id = undefined
+    console.log(from)
+    await ctx.reply(`Olá ${from.username}, o seu nome é: ${from.first_name} ${from.last_name}!`)
+})
+
+//Evento de texto
+bot.on('text', ctx => ctx.reply('Alo Ha Mundo SENAI 2020'))
+
+bot.launch()
