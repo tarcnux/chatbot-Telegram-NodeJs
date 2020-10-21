e6979006fb762e469ae9532e3038e3100abecc3d
TARCISIO NUNES <tarcnux@gmail.com>
2020-10-21 20:16:14 -0300

    Criar primeiro bot que responde texto

---
+++ bot_01.js
@@ -0,0 +1,15 @@
+const env = require('./.env')
+const Telegraf = require('telegraf')
+const bot = new Telegraf(env.token)
+
+bot.use(async (ctx, next) => {
+    const start = new Date()
+    await next()
+    const ms = new Date - start
+    const dataEHora = new Date().toLocaleString();
+    console.log(`${dataEHora} \n Tempo de resposta: ${ms}ms`)
+})
+
+bot.on('text', ctx => ctx.reply('Alo Ha Mundo SENAI 2020'))
+
+bot.launch()
